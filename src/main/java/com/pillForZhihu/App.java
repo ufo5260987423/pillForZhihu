b05aa144bccd08e8ff27f8f57e4c3267ddccb539
ufo <ufo5260987423@163.com>
2015-06-11 11:00:22 +0800

    init ignore and readme and doc

---
+++ src/main/java/com/pillForZhihu/App.java
@@ -0,0 +1,13 @@
+package com.pillForZhihu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
