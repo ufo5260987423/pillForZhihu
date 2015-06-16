6d0ad08a9152b4718162865f8803538ddc67ab30
ufo <ufo5260987423@163.com>
2015-06-16 08:59:48 +0800

    just complete some dao

---
+++ webApp/src/main/java/com/pillForZhihu/webApp/dao/Content_key.java
@@ -0,0 +1,8 @@
+package com.pillForZhihu.webApp.dao;
+
+import com.pillForZhihu.webApp.dao.utils.KeyBase;
+
+/**
+ * Created by ufo on 6/16/15.
+ */
+public class Content_key extends KeyBase{ }
---
+++ webApp/src/main/java/com/pillForZhihu/webApp/dao/Content_value.java
@@ -0,0 +1,8 @@
+package com.pillForZhihu.webApp.dao;
+
+import com.pillForZhihu.webApp.dao.utils.ValueBase;
+
+/**
+ * Created by ufo on 6/16/15.
+ */
+public class Content_value extends ValueBase { }
